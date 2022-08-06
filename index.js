9c169467d91dec366463e0c3a228ddbfa9f7a064
Manuel Vilche <manuel@fizzmod.com>
2022-08-06 19:14:42 -0300

    improve reboot and add stop

--- index.js
+++ index.js
@@ -6,9 +6,9 @@ const express = require('express');
 const logger = require('lllog')();
 
 // Load the AWS SDK for Node.js
-var AWS = require('aws-sdk');
+const AWS = require('aws-sdk');
 // Set the region
-AWS.config.update({region: process.env.AWS_DEFAULT_REGION || 'us-east-1'});
+AWS.config.update({ region: process.env.AWS_DEFAULT_REGION || 'us-east-1' });
 
 const packageInfo = require('./package.json');
 
@@ -20,50 +20,83 @@ app.get('/', (req, res) => {
 	res.json({ version: packageInfo.version });
 });
 
+const updateDNS = async (instanceId, ec2) => {
+	ec2.describeInstances({ InstanceIds: [instanceId] }, (err, data) => {
+
+		if(err)
+			console.log(err);
+
+		console.log('id:', data.Reservations[0].Instances[0].PublicIpAddress);
+	});
+
+}
+
 app.post('/restart-instance', async (req, res) => {
 
 	const { instanceId, hash } = req.body;
-	console.log('body:', req.body);
 
 	if(!process.env.HASH || process.env.HASH !== hash)
 		return res.status(403).send('Invalid credentials');
 
 	if(!instanceId)
-		return res.status(400).send('You most send the instanceId');
+		return res.status(400).send('You has to send the instanceId');
 
-	logger.info('sin errores');
 	// Create EC2 service object
-	var ec2 = new AWS.EC2({apiVersion: '2016-11-15'});
-	logger.info(ec2);
+	const ec2 = new AWS.EC2({ apiVersion: '2016-11-15' });
 
-	var params = {
+	// Call EC2 to reboot instances
+	const params = {
 		InstanceIds: [instanceId]
 	};
 
-	logger.info(params);
-	// Call EC2 to reboot instances
-	ec2.rebootInstances(params, function(err, data) {
-		console.log('Arrannco con el rebootInstances');
-		console.log('err:', err);
-		console.log('data:', data);
-		// if(err && err.code === 'DryRunOperation') {
-		// 	logger.info(params);
-		// 	params.DryRun = false;
-			ec2.rebootInstances(params, function(err, data) {
-				logger.info(data);
-				if(err)
-					console.log("Error", err);
-				else if (data) {
-					logger.info('data:', data);
-					res.json(`instanceId: ${instanceId} restarted`);
-				}
+	ec2.rebootInstances(params, async (err, data) => {
+		let message = 'restarted';
+		if(err) {
+			console.log('Error', err);
 
-			});
-		// } else
-		// 	console.log("You don't have permission to reboot instances.");
+			if(err.code === 'IncorrectState') {
+
+				await ec2.startInstances(params, (startError, startData) => {
+
+					if(startError)
+						console.log('Error', startError);
+
+					logger.info('CurrentState:', startData.StartingInstances[0].CurrentState.Name);
+					logger.info('PreviousState:', startData.StartingInstances[0].PreviousState.Name);
+					message = 'started';
+				});
+			}
+		}
+
+		await updateDNS(instanceId, ec2);
+		res.json(`instanceId: ${instanceId} ${message}`);
 	});
 
+});
 
+app.post('/stop', async (req, res) => {
+
+	const { instanceId, hash } = req.body;
+
+	if(!process.env.HASH || process.env.HASH !== hash)
+		return res.status(403).send('Invalid credentials');
+
+	if(!instanceId)
+		return res.status(400).send('You has to send the instanceId');
+
+	// Create EC2 service object
+	const ec2 = new AWS.EC2({ apiVersion: '2016-11-15' });
+
+	// Call EC2 to reboot instances
+	ec2.stopInstances({ InstanceIds: [instanceId] }, (err, response) => {
+
+		if(err)
+			console.log('Error', err);
+
+		logger.info('CurrentState:', response.StoppingInstances[0].CurrentState.Name);
+		logger.info('PreviousState:', response.StoppingInstances[0].PreviousState.Name);
+		res.json(`instanceId: ${instanceId} stopped`);
+	});
 });
 
 const PORT = process.env.PORT || 3000;
