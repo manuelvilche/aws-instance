29b4def13b2d7e05fd9553a452ee06bb97638f90
Manuel Vilche <manuel@fizzmod.com>
2022-08-07 22:22:45 -0300

    add describe instance

--- index.js
+++ index.js
@@ -20,16 +20,41 @@ app.get('/', (req, res) => {
 	res.json({ version: packageInfo.version });
 });
 
-const updateDNS = async (instanceId, ec2) => {
-	ec2.describeInstances({ InstanceIds: [instanceId] }, (err, data) => {
+const updateDNS = async (instanceId, ec2, retries = 0) => {
 
-		if(err)
-			console.log(err);
+	console.log('getting ip address');
 
-		console.log('id:', data.Reservations[0].Instances[0].PublicIpAddress);
-	});
+	return new Promise((resolve, reject) => {
 
-}
+		return ec2.describeInstances({ InstanceIds: [instanceId] }, async (err, data) => {
+
+			if(err)
+				console.log(err);
+
+			const ip = data.Reservations[0].Instances[0].PublicIpAddress;
+
+			console.log('ip:', ip);
+
+			if(retries >= 3) {
+				console.log('too match tries');
+				resolve();
+			}
+
+			if(!ip) {
+				retries++;
+				console.log(`retry: ${retries}`);
+
+				console.log('sleeping for 5 seconds');
+				await new Promise(resolve => setTimeout(resolve, 5000));
+				await updateDNS(instanceId, ec2, retries);
+			}
+
+			console.log('PublicIpAddress:', data.Reservations[0].Instances[0].PublicIpAddress);
+			resolve();
+		});
+
+	})
+};
 
 app.post('/restart-instance', async (req, res) => {
 
@@ -51,8 +76,9 @@ app.post('/restart-instance', async (req, res) => {
 
 	ec2.rebootInstances(params, async (err, data) => {
 		let message = 'restarted';
+
 		if(err) {
-			console.log('Error', err);
+			// console.log('Error', err);
 
 			if(err.code === 'IncorrectState') {
 
@@ -69,7 +95,8 @@ app.post('/restart-instance', async (req, res) => {
 		}
 
 		await updateDNS(instanceId, ec2);
-		res.json(`instanceId: ${instanceId} ${message}`);
+
+		return res.json(`instanceId: ${instanceId} ${message}`);
 	});
 
 });
@@ -96,6 +123,29 @@ app.post('/stop', async (req, res) => {
 		logger.info('CurrentState:', response.StoppingInstances[0].CurrentState.Name);
 		logger.info('PreviousState:', response.StoppingInstances[0].PreviousState.Name);
 		res.json(`instanceId: ${instanceId} stopped`);
+	});
+});
+
+app.post('/describe-instance', async (req, res) => {
+
+	const { instanceId, hash } = req.body;
+
+	if(!process.env.HASH || process.env.HASH !== hash)
+		return res.status(403).send('Invalid credentials');
+
+	if(!instanceId)
+		return res.status(400).send('You has to send the instanceId');
+
+	// Create EC2 service object
+	const ec2 = new AWS.EC2({ apiVersion: '2016-11-15' });
+
+	// Call EC2 to describe instances
+	ec2.describeInstances({ InstanceIds: [instanceId] }, (err, response) => {
+
+		if(err)
+			console.log('Error', err);
+
+		res.json(response);
 	});
 });
 
